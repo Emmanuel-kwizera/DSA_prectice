65993b68696a5ca4af5d4e20a3f2f5673cd13721
Kwizera Emmanuel <kwizemmy2004@gmail.com>
2022-06-10 20:22:41 +0000

    checking equlity

--- Basics/subArray.cpp
+++ Basics/subArray.cpp
@@ -32,13 +32,46 @@ void print_subarrays(int arr[],int n,int i,int j) {
     // cout <<"\n \n \n " <<print_arr(arr,n,i,j);
 }
 
-
+bool check_Exsistance(int n , int array[], int size){
+    for(int i=0;i<size;i++){
+        // cout << array[i] <<endl;
+        if(array[i] == n){
+            return true;
+        }    
+    }
+    return false; 
+}
 
 int main(){
     int arr[5] = {2,5,7,4},n = 4; 
       
-    cout << "All sub arrays of the array: " <<endl;
+    // cout << "All sub arrays of the array: " <<endl;
 
-	print_subarrays(arr,n,0,0); 
+	// print_subarrays(arr,n,0,0); 
+
+    // int number;
+    // std::cout<<"Enter a sum to seach for : ";
+    // std::cin >> number;
+    // std::cout << "Does sub array sums containt [ " << number <<" ]" <<endl;
+
+    int x;
+    cout<<"     [ ";
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" "; 
+    }
+    cout<<"] ";
+
+    while(1){
+        std::cout << "\n Enter a number to seach for : ";
+        std::cin >> x;
+        if(x == -1 )
+            exit(0);
+        else{
+            std::cout << "\n ======================================================= \n\t";
+            std::cout <<"Return of check_Exsistance: " << check_Exsistance(x,arr,n);
+            std::cout << "\n ======================================================= \n ";
+        }
+    }
+
     return EXIT_SUCCESS;
 }
